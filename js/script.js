ddc471fa2a01dd9a1c45152fa5b58842c311d4e7
Olha Yaroshevych <jaroshewytsch.olia@gmail.com>
2022-01-12 14:25:53 +0200

    Layout_3

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		initialSlide:0,
+		infinite:true,
+		autoplay:true,
+		autoplaySpeed: 4000,
+		pauseOnFocus:true,
+		pauseOnHover:true,
+		fade:true,		
+	});
+
+});
